75e9c78607f595c064fae1f8771e9ffc6be495fc
UmurerwaDivine <divinekagame@gmail.com>
2018-12-21 13:18:58 +0200

    edited condition

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
+function answer() {
+    var option = document.board.option.value;
+    var optionOne = document.board.optionOne.value;
+    var optionTwo = document.board.optionTwo.value;
+    var optionThree = document.board.optionThree.value;
+    var optionFour = document.board.optionFour.value;
+    var opt = 0;
 
-    $(document).ready(function(){
-        $("#board").submit(function (event) {
-     
-    $('#output').text('');
-    
+//if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
+   // $('#questionsIncomplete').text("Please Complete questions Before Submitting");
 
-var option = $("input:radio[name=option]:checked ").val();
-var optionOne = $("input:radio[name=optionOne]:checked").val();
-var optionTwo = $("input:radio[name=optionTwo]:checked").val();
-var optionThree = $("input:radio[name=optionThree]:checked").val();
-var optionFour = $("input:radio[name=optionFour]:checked").val();
-var opt = 0;
-
-
-if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
-    $('#questionsIncomplete').text("Please Complete questions Before Submitting");
-
-    }else{
+    //}else{
 
    
 if (option == "Method") {
@@ -34,26 +27,12 @@ if ( optionThree == "expressions") {
 if ( optionFour == "Based") {
     opt = opt + 10;
 };
-$("input:radio[name=option]:checked ").prop('checked', false);
-$("input:radio[name=optionOne]:checked").prop('checked', false);
-$("input:radio[name=optionTwo]:checked").prop('checked', false);
-$("input:radio[name=optionThree]:checked").prop('checked', false);
- $("input:radio[name=optionFour]:checked").prop('checked', false);
+   // }
 
 
-}
-$('#questionsIncomplete').text('');
-        $('#output').text(result);
+document.getElementById("output").style.visibility = "visible";
+document.getElementById("correct").innerHTML = "Your Total Marks: " + opt + " /50";
+$(".container").fadeOut();
 
-        event.preventDefault();
-
- });
-});
-
-//document.getElementById("output").style.visibility = "visible";
-//document.getElementById("correct").innerHTML = "Your Total Marks " + opt + " /50";
-
-
-   // $(".container").fadeOut();
-
+};
 
