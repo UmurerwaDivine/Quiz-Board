52fb83012281ca0bd2956b2d7fbac23731a686aa
UmurerwaDivine <divinekagame@gmail.com>
2018-12-21 11:44:47 +0200

    added conditions in file js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 function result(){
-    
-}
+
+
 var option = $("input:radio[name=option]:").val();
 var optionOne = $("input:radio[name=optionOne]:").val();
 var optionTwo = $("input:radio[name=optionTwo]:").val();
 var optionThree = $("input:radio[name=optionThree]:").val();
 var optionFour = $("input:radio[name=optionFour]:").val();
+var opt = 0;
+
+if (option == "Method") {
+    opt = opt + 10;
+};
+if (optionOne == "Division") {
+    opt =opt+ 10;
+};
+if (optionTwo  == "literal") {
+    opt = opt + 10;
+};
+if ( optionThree == "expressions") {
+    opt = opt + 10;
+};
+if ( optionFour == "Based") {
+    opt = opt + 10;
+};
+
+}
 
 $(document).ready(function() {
     $("form#board").submit(function(event) {
